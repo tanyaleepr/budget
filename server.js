24fe8c80e142065cc0d5c799206d66d652e2d577
tanyaleepr <tanyaleepr@gmail.com>
2022-05-07 12:44:29 -0500

    heroku issue 1

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
+mongoose.connect(`mongodb://${process.env.DB_NAME}:${process.env.DB_PASS}@ds241658.mlab.com:41658/pwa-test`,(err)=>{
+if(err) throw err;
+console.log("DB Connected Successfully");
+});
 const app = express();
 
 app.use(logger("dev"));
@@ -18,8 +23,7 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+  useFindAndModify: false
 });
 
 // routes
